a32536d5b6590f6ff8d887db829725e56a7cd0de
5m1Ly <hbouma01@gmail.com>
2023-04-13 23:43:39 +0200

    updated the script

--- source/index.js
+++ source/index.js
@@ -1,60 +1,114 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const File = require('./File');
+class Resource {
 
-let dircount = 0;
-let catagories = 0;
-let resources = 0;
-let size = 0;
-
-function gatherFileInfo(parent = 'server', current = 'resources', depth = 0) {
-	const dir = path.join(parent, current);
-	const stat = fs.statSync(dir);
-	let files = [];
-	if (stat.isFile()) {
-		if (current === "__resource.lua") {
-			// const resource = path.basename(path.dirname(dir))
-			resources++;
-		}
-		size += stat.size;
-		return [new File(dir, stat)];
-	} else if (stat.isDirectory()) {
-		dircount++;
-		if (current.match(/^\[.*\]$/)) {
-			catagories++;
-		}
-		const content = fs.readdirSync(dir);
-		for (const item of content) {
-			let rfiles = gatherFileInfo(dir, item, depth + 1);
-			files.push(...rfiles);
-		}
-	} else {
-		console.log('there is no file nor directory to gather information about at:', dir);
+	constructor(name) {
+		this.name = name;
+		this.files = 0;
+		this.size = 0;
 	}
-	return files;
+
+	addFile() {
+		this.files++;
+	}
+
+	addSize(size) {
+		this.size += size;
+	}
+
+	setModified(modified) {
+		if (!this.modifications) {
+			this.modifications = {
+				latest: modified,
+				oldest: modified,
+			}
+		} else if (modified > this.modifications.latest) {
+			this.modifications.latest = modified;
+		} else if (modified < this.modifications.oldest) {
+			this.modifications.oldest = modified;
+		}
+	}
 }
 
-const files = gatherFileInfo().sort((f1, f2) => (f1.modified() - f2.modified()));
+function gatherResourceInfo(current = 'server/resources', resource = null, resources = new Map(), depth = 0) {
+
+	const currentStats = fs.statSync(current);
+
+	if (currentStats.isDirectory()) {
+
+		const content = fs.readdirSync(current);
+
+		if (resource == null && content.includes('__resource.lua')) {
+			resource = path.basename(current);
+		}
+
+		for (const item of content) {
+			resources = gatherResourceInfo(path.join(current, item), resource, resources, depth + 1);
+		}
+
+	}
+
+	if (currentStats.isFile()) {
+
+		const instance = resources.has(resource) ? resources.get(resource) : new Resource(resource);
+
+		instance.addFile();
+		instance.addSize(currentStats.size);
+		instance.setModified(currentStats.mtime);
+
+		resources.set(resource, instance)
+
+	}
+
+	if (depth) {
+		return resources;
+	} else {
+		return [...resources.values()];
+	}
+
+}
+
+const resources = gatherResourceInfo().sort((r1, r2) => (r1.modifications.latest - r2.modifications.latest));
+
 
 let markdown = `# All files and modification dates in the server/resources directory
 
-> **Some stats of the resources directory:**
-> files: ${files.length}
-> directories: ${dircount}
+> **Some stats of the resources directory:**<br/>
+> files: 11803<br/>
+> directories: 788<br/>
 >
-> catagories: ${catagories}
-> resources: ${resources}
+> catagories: 52<br/>
+> resources: 244
 >
-> size: ${Math.ceil(size / 10000000) / 100}gb
+> size: 6.63gb
 >
 > time format: Central European Summer Time (CEST, GMT+2)
 
-| file | last mod date | last mod time | size | directory |
-| :--- | ------------: | ------------: | :--: | :-------- |`;
+| resource | files         | size | latest modification | oldest modification |
+| :------- | ------------: | ---: | :-----------------: | :------------ |`;
 
-for (const file of files) {
-	markdown += file.markdownTableRow();
+const gigabyte = 1000000000;
+const megabyte = 1000000;
+const kilobyte = 1000;
+const byte = 1;
+
+for (const resource of resources) {
+
+	const latest = new Date(resource.modifications.latest).toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" });
+	const oldest = new Date(resource.modifications.oldest).toLocaleDateString('en-us', { year: "numeric", month: "short", day: "numeric" });
+
+	if (resource.size > gigabyte) {
+		resource.size = `${(resource.size / gigabyte).toFixed(2)}gb`;
+	} else if (resource.size > megabyte) {
+		resource.size = `${(resource.size / megabyte).toFixed(2)}mb`;
+	} else if (resource.size > kilobyte) {
+		resource.size = `${(resource.size / kilobyte).toFixed(2)}kb`;
+	} else {
+		resource.size = `${resource.size}b`;
+	}
+
+	markdown += `\n| ${resource.name} | ${resource.files} | ${resource.size} | ${latest} | ${oldest} |`;
 }
 
 fs.writeFileSync(path.join(__dirname, '../modification_dates.md'), markdown);
